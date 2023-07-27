0e67f55f5704a42cf213bfb1062e448be09e65fc
nassirou <nassirou9993@gmail.com>
2023-07-27 17:17:01 +0100

    the hero section

---
+++ src/components/Hero.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Crypto from '../assets/trade.png'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className='container'>
+            {/* */}
+            <div className='left'>
+                <p>By & Sell Crypto 24/7 using your retirement account</p>
+                <h1>Invest in Cryptocurreny with Your IRA</h1>
+                <p>Buy, Sell, and store hundreds of crytocurrencies</p>
+                <div className='input-container'>
+                    <input type="email" placeholder="Enter your email" />
+                    <button className='btn btn_hero'>Learn More</button>
+                </div>
+            </div>
+
+            {/* */}
+            <div className='rigth'>
+                <div className=''>
+                    <div className="img-container">
+                        <img src={Crypto} alt='/'/>
+                    </div>
+                </div>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Hero
